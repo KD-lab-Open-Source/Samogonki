5bbd527909c9fbc69d8590dc1ae948b41c72bca8
Ivan Ushakov <ivan.ushakov@gmail.com>
2023-11-11 13:23:50 +0300

    Basic Steam SDK support

--- MechoSoma/app/main.cpp
+++ MechoSoma/app/main.cpp
@@ -9,6 +9,15 @@
 #define SOKOL_GLCORE33
 #endif
 
+#ifdef STEAM_VERSION
+#include <algorithm>
+#include <array>
+#include <string_view>
+#include <utility>
+
+#include <steam/steam_api.h>
+#endif
+
 #define SOKOL_GFX_IMPL
 #include <sokol_gfx.h>
 
@@ -17,12 +26,36 @@
 int xtSysQuantDisabled = 0;
 
 int main(int argc, char const *argv[]) {
+  #ifdef STEAM_VERSION
+  if (!SteamAPI_Init()) {
+    return 1;
+  }
+
+  using LanguagePair = std::pair<std::string_view, std::string_view>;
+  constexpr std::array<LanguagePair, 4> map{
+    LanguagePair("english", "en"),
+    LanguagePair("czech", "cz"),
+    LanguagePair("italian", "it"),
+    LanguagePair("russian", "ru")
+  };
+
+  auto language = SteamApps()->GetCurrentGameLanguage();
+  const auto p = std::find_if(map.begin(), map.end(), [&language](const auto &e) {
+    return e.first == language;
+  });
+  if (p == map.end()) {
+    return 1;
+  }
+
+  localization::setLanguage(p->second.data());
+  #else
   for (int i = 1; i < argc - 1; ++i) {
       if (strcmp(argv[i], "-lang") == 0 && strlen(argv[i + 1]) == 2) {
           localization::setLanguage(argv[i + 1]);
           break;
       }
   }
+  #endif
 
   SDL_Init(SDL_INIT_VIDEO);
 
