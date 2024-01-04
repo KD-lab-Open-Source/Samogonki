a21cfac5618d49fbbbe32e507d33d9142a74e829
Vitaliy Busko <vitaliy.opensource@gmail.com>
2024-01-05 06:21:57 +0700

    feat: basic network support

--- MechoSoma/RUNTIME/wininet_api.cpp
+++ MechoSoma/RUNTIME/wininet_api.cpp
@@ -1,15 +1,18 @@
 
+
+
 /* ---------------------------- INCLUDE SECTION ----------------------------- */
 
-#include "StdAfx.h"
+// #include "StdAfx.h"
 
-#include <windows.h>
-#include <wininet.h>
-
+#include <string>
 #include <stdio.h>
 
+#include <curl/curl.h>
+
 #include "wininet_api.h"
-#include "Xreal_utl.h"
+// #include "Xreal_utl.h"
+
 
 /* ----------------------------- STRUCT SECTION ----------------------------- */
 
@@ -21,99 +24,55 @@ typedef struct
 /* ----------------------------- EXTERN SECTION ----------------------------- */
 /* --------------------------- PROTOTYPE SECTION ---------------------------- */
 
-void __stdcall wiStatus(HINTERNET hInternet,DWORD dwContext,DWORD dwInternetStatus,LPVOID lpvStatusInformation,DWORD dwStatusInformationLength);
-char* win32_errmsg(int id);
-
 /* --------------------------- DEFINITION SECTION --------------------------- */
 
-//#define _ASYNC_MODE_
-
-HINTERNET hIntRoot = NULL;
-
-char* wiServerName = NULL;
-char* wiGameURL = NULL;
+const char* wiServerName = NULL;
+const char* wiGameURL = NULL;
 int wiServerPort = 8500;
 
 wiDispatcher wi_D;
 
-CRITICAL_SECTION wiCriticalSec;
 void* wiCritical = NULL; 
+
+// cURL multi-handle
+// static CURLM* main_handle = NULL;
+// static std::string endpoint;
 
 void wiInit(void)
 {
-	static char* agent = "Spanking Runners";
-
-#ifdef _ASYNC_MODE_
-	InitializeCriticalSection(&wiCriticalSec);
-	wiCritical = &wiCriticalSec;
-
-	hIntRoot = InternetOpen(agent,INTERNET_OPEN_TYPE_PRECONFIG,NULL,NULL,INTERNET_FLAG_ASYNC);
-	InternetSetStatusCallback(hIntRoot,(INTERNET_STATUS_CALLBACK)wiStatus);
-
-	CONTROL_FP();
-#else
-	hIntRoot = InternetOpen(agent,INTERNET_OPEN_TYPE_PRECONFIG,NULL,NULL,0);
-
-	CONTROL_FP();
-#endif
+	// main_handle = curl_multi_init();
+	// endpoint
 }
 
 void wiFinit(void)
 {
-	if(hIntRoot)
-		InternetCloseHandle(hIntRoot);
+	//TODO:
+	// 	Cleans up and removes a whole multi stack. It does not free or touch any individual easy handles in any way - they still need to be closed individually, using the usual curl_easy_cleanup way. The order of cleaning up should be:
+	// [O] 1 - curl_multi_remove_handle before any easy handles are cleaned up
+	// [O] 2 - curl_easy_cleanup can now be called independently since the easy handle is no longer connected to the multi handle
+	// [X] 3 - curl_multi_cleanup should be called when all easy handles are removed
+	// Passing in a NULL pointer in multi_handle makes this function return CURLM_BAD_HANDLE immediately with no other action. 
 
-	CONTROL_FP();
-}
-
-void __stdcall wiStatus(HINTERNET hInternet,DWORD dwContext,DWORD dwInternetStatus,LPVOID lpvStatusInformation,DWORD dwStatusInformationLength)
-{
-	REQUEST_CONTEXT* cpContext;
-
-	wiCriticalSection sec(wiCritical);
-
-	cpContext = (REQUEST_CONTEXT*)dwContext;
-	
-	switch(dwInternetStatus){
-		case INTERNET_STATUS_REQUEST_COMPLETE:
-			if(LPINTERNET_ASYNC_RESULT(lpvStatusInformation) -> dwError == 0)
-				cpContext -> status = dwInternetStatus;
-			else 
-				cpContext -> status = -1;
-			break;
-		default:
-			cpContext -> status = dwInternetStatus;
-			break;
-	}
+	// curl_multi_cleanup(main_handle);
 }
 
 wiDispatcher::wiDispatcher(void)
 {
-	flags = 0;
+	is_request_completed = false;
 	status = WI_IDLE;
-
-	hConnect = NULL;
-	hRequest = NULL;
-
-	reqContext = new REQUEST_CONTEXT;
-	memset(reqContext,0,sizeof(REQUEST_CONTEXT));
 
 	inPos = inSize = 0;
 	inBuffer = NULL;
 
 	outPos = outSize = 0;
 	outBuffer = NULL;
+
+	hCurlMulti = curl_multi_init();
 }
 
 wiDispatcher::~wiDispatcher(void)
 {
-	close_request();
-	disconnect();
-
-	free_inbuf();
-	free_outbuf();
-
-	delete reqContext;
+	curl_multi_cleanup(hCurlMulti);
 }
 
 void wiDispatcher::expand_inbuf(int sz)
@@ -125,7 +84,7 @@ void wiDispatcher::expand_inbuf(int sz)
 		memset(buf,0,sz + 1);
 		memcpy(buf,inBuffer,inSize);
 
-		delete inBuffer;
+		delete[] inBuffer;
 		
 		inBuffer = buf;
 		inSize = sz + 1;
@@ -149,7 +108,7 @@ void wiDispatcher::free_inbuf(void)
 {
 	if(inSize){
 		inSize = 0;
-		delete inBuffer;
+		delete[] inBuffer;
 	}
 }
 
@@ -170,264 +129,151 @@ void wiDispatcher::free_outbuf(void)
 {
 	if(outSize){
 		outSize = 0;
-		delete outBuffer;
+		delete[] outBuffer;
 	}
 }
 
-int wiDispatcher::connect(char* server,unsigned int port)
+int wiDispatcher::connect(const char* server,unsigned int port)
 {
-	REQUEST_CONTEXT* rq = (REQUEST_CONTEXT*) reqContext;
+	stringstream ss;
+	ss
+		<< "http" << "://"
+		<< server << ":" << port;
 
-	disconnect();
+	endpoint = ss.str();
+	
+	printf("Connect to: %s", endpoint);
 
-	if(port == 0xFFFFFFFF)
-		port = INTERNET_DEFAULT_HTTP_PORT;
-
-#ifdef _ASYNC_MODE_
-	hConnect = InternetConnect(hIntRoot,server,port,NULL,NULL,INTERNET_SERVICE_HTTP,0,(DWORD)reqContext);
-
-	CONTROL_FP();
-#else
-	DWORD cnt = 0;
-	hConnect = InternetConnect(hIntRoot,server,port,NULL,NULL,INTERNET_SERVICE_HTTP,0,cnt);
-
-	CONTROL_FP();
-#endif
-
-	if(hConnect) return 1;
-
-	return 0;
+	return 1;
 }
 
 void wiDispatcher::disconnect(void)
 {
-	if(hConnect) InternetCloseHandle(hConnect);
-	hConnect = NULL;
-
-	CONTROL_FP();
 }
 
-int wiDispatcher::open_request(int action,char* obj,char* header,int header_len,char* data,int data_len)
+static void progress(void *contents, size_t size, size_t nmemb, vector<char>* buff) {
+	printf("callback curl called, input len: %d\r\n", nmemb);
+	for (int i = 0; i < nmemb; i++) {
+		buff->push_back(*(i + (char *)contents));
+	}
+}
+
+int wiDispatcher::open_request(int action,const char* obj,const char* header,int header_len,const char* data,int data_len)
 {
-	unsigned long idx = 0;
+	string url = endpoint + string(obj);
 
-	char* verb = (action == WI_POST) ? "POST" : "GET";
+	printf("wiDispatcher::open_request, URL: %s\r\n", url);
 
-	if(status != WI_IDLE) close_request();
+	if (status != WI_IDLE) {
+		close_request();
+	}
 
-	if(!hConnect) return 0;
+	this->iBuffer.clear();
 
-	inPos = outPos = 0;
-
-#ifdef _ASYNC_MODE_
-	if(!(hRequest = HttpOpenRequest(hConnect,verb,obj,NULL,NULL,NULL,INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_RELOAD,(DWORD)reqContext)))
+	if (action != WI_POST && action != WI_GET) {
 		return 0;
-#else
-	DWORD cnt = 0;
-	if(!(hRequest = HttpOpenRequest(hConnect,verb,obj,NULL,NULL,NULL,INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_RELOAD,cnt)))
+	}
+
+	CURL *handle = curl_easy_init();
+	if (!handle) {
 		return 0;
-#endif
+	}
 
-	HttpSendRequest(hRequest,header,header_len,data,data_len);
+	int running_handles = 0;
+	curl_easy_setopt(handle, CURLOPT_URL, url.data());
 
+	if (action == WI_POST) {
+		curl_easy_setopt(handle, CURLOPT_POST, 1);
+		curl_easy_setopt(handle, CURLOPT_POSTFIELDS, data);
+	}
+	
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, &this->iBuffer);
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, progress);
+		// curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, [](void *contents, size_t size, size_t nmemb, wiDispatcher *self) {
+		// 	printf("callback curl called, input len: %d\r\n", nmemb);
+		// 	for (int i = 0; i < nmemb; i++) {
+		// 		self->iBuffer.push_back(*((char *)contents));
+		// 	}
+		// });
+
+	curl_multi_add_handle(hCurlMulti, handle);
+	curl_multi_perform(hCurlMulti, &running_handles);
+	if (running_handles == 0) {
+		action = WI_ERROR;
+		// TODO: free curl handles
+		throw "error occured when trying to send a request";
+	}
+
+	hCurlRequest = handle;
+	is_request_completed = false;
 	status = action;
-
-	flags &= ~WI_REQUEST_COMPLETED;
-
-#ifndef _ASYNC_MODE_
-	REQUEST_CONTEXT *rc = (REQUEST_CONTEXT*)reqContext;
-	rc -> status = INTERNET_STATUS_REQUEST_COMPLETE;
-#endif
-
-	CONTROL_FP();
 
 	return 1;
 }
 
 void wiDispatcher::close_request(void)
 {
-	if(hRequest)
-		InternetCloseHandle(hRequest);
+	if (hCurlRequest) {
+		curl_multi_remove_handle(hCurlMulti, hCurlRequest);
+		curl_easy_cleanup(hCurlRequest);
+		hCurlRequest = NULL;
+	}
 
-	hRequest = 0;
-	flags &= ~WI_REQUEST_COMPLETED;
+	// iBuffer.clear();
+	// oBuffer.clear();
+
+	is_request_completed = false;
 	status = WI_IDLE;
-
-	CONTROL_FP();
 }
 
 void wiDispatcher::quant(void)
 {
-	unsigned long sz,dl_sz;
-	REQUEST_CONTEXT* rc;
+	printf("wiDispatcher::quant\r\n");
 
-	wiCriticalSection sec(wiCritical);
-
-	rc = (REQUEST_CONTEXT*)reqContext;
+	int running_handles = 0;
 
 	if(status == WI_LOAD_FILE){
 		status = WI_FINISHED;
 		return;
 	}
-
-	if(status == WI_GET){
-		if(flags & WI_REQUEST_COMPLETED){
-			if(!InternetQueryDataAvailable(hRequest,&sz,0,0)){
-				return;
-			}
-			if(sz){
-				if(sz > WI_TEMP_BUFFER_SIZE) sz = WI_TEMP_BUFFER_SIZE;
-
-				if((int)sz + inPos >= inSize)
-					sz = inSize - inPos;
-
-				if(!InternetReadFile(hRequest,(LPVOID)(inBuffer + inPos),sz,&dl_sz)){
-					set_error();
-					return;
-				}
-
-				inPos += dl_sz;
-				if(inPos >= inSize)
-					status = WI_FINISHED;
-			}
-			else 
-				status = WI_FINISHED;
+	else if(status == WI_GET || status == WI_POST){
+		curl_multi_perform(hCurlMulti, &running_handles);
+		printf("wiDispatcher::quant running handles: %d\r\n", running_handles);
+		if (running_handles > 0) {
 			return;
-		}
+		} else {
+			iBuffer.push_back(0);
+			alloc_inbuf(iBuffer.capacity());
+			memcpy(inBuffer, iBuffer.data(), iBuffer.capacity());
 
-		if(rc -> status == -1){
-			set_error();
-			return;
-		}
-		if(rc -> status == INTERNET_STATUS_REQUEST_COMPLETE){
-			if(!InternetQueryDataAvailable(hRequest,&sz,0,0)){
-				set_error();
-				return;
+			if (hCurlRequest) {
+				curl_multi_remove_handle(hCurlMulti, hCurlRequest);
+				curl_easy_cleanup(hCurlRequest);
+				hCurlRequest = NULL;
 			}
 
-			if(sz)
-				alloc_inbuf(sz);
-
-			flags |= WI_REQUEST_COMPLETED;
+			status = WI_FINISHED;
+			is_request_completed = true;
 		}
-	}
-	if(status == WI_POST){
-		if(flags & WI_REQUEST_COMPLETED){
-			if(!InternetQueryDataAvailable(hRequest,&sz,0,0)){
-				return;
-			}
-			if(sz){
-				if(sz > WI_TEMP_BUFFER_SIZE) sz = WI_TEMP_BUFFER_SIZE;
-
-				if((int)sz + inPos >= inSize)
-					expand_inbuf((int)sz + inPos);
-
-				if(!InternetReadFile(hRequest,(LPVOID)(inBuffer + inPos),sz,&dl_sz)){
-					set_error();
-					return;
-				}
-
-				inPos += dl_sz;
-				if(inPos >= inSize)
-					status = WI_FINISHED;
-			}
-			else 
-				status = WI_FINISHED;
-			return;
-		}
-		if(rc -> status == -1){
-			set_error();
-			return;
-		}
-		if(rc -> status == INTERNET_STATUS_REQUEST_COMPLETE){
-			InternetQueryDataAvailable(hRequest,&sz,0,0);
-
-			if(sz)
-				alloc_inbuf(sz);
-
-			flags |= WI_REQUEST_COMPLETED;
-		}
-
 	}
 }
 
 int wiDispatcher::get_request_status(void)
 {
-	return (flags & WI_REQUEST_COMPLETED);
+	return is_request_completed;
 }
 
-char* wiDispatcher::get_request_status_str(void)
+const char* wiDispatcher::get_request_status_str(void)
 {
-	REQUEST_CONTEXT *rc = (REQUEST_CONTEXT*)reqContext;
-
-	static char* status_msg[] = {
-		"closing connection",
-		"connected to server",
-		"connecting to server",
-		"connection closed",
-		"handle closing",
-		"handle created",
-		"intermediate response",
-		"name resolved",
-		"receiving response",
-		"response received",
-		"redirect",
-		"request sent",
-		"resolving name",
-		"sending request",
-		"state change",
-		"request complete",
-		" "
-	};
-	switch(rc -> status){
-		case INTERNET_STATUS_CLOSING_CONNECTION:
-			return status_msg[0];
-		case INTERNET_STATUS_CONNECTED_TO_SERVER:
-			return status_msg[1];
-		case INTERNET_STATUS_CONNECTING_TO_SERVER:
-			return status_msg[2];
-		case INTERNET_STATUS_CONNECTION_CLOSED:
-			return status_msg[3];
-		case INTERNET_STATUS_HANDLE_CLOSING:
-			return status_msg[4];
-		case INTERNET_STATUS_HANDLE_CREATED:
-			return status_msg[5];
-		case INTERNET_STATUS_INTERMEDIATE_RESPONSE:
-			return status_msg[6];
-		case INTERNET_STATUS_NAME_RESOLVED:
-			return status_msg[7];
-		case INTERNET_STATUS_RECEIVING_RESPONSE:
-			return status_msg[8];
-		case INTERNET_STATUS_RESPONSE_RECEIVED:
-			return status_msg[9];
-		case INTERNET_STATUS_REDIRECT:
-			return status_msg[10];
-		case INTERNET_STATUS_REQUEST_SENT:
-			return status_msg[11];
-		case INTERNET_STATUS_RESOLVING_NAME:
-			return status_msg[12];
-		case INTERNET_STATUS_SENDING_REQUEST:
-			return status_msg[13];
-		case INTERNET_STATUS_STATE_CHANGE:
-			return status_msg[14];
-		case INTERNET_STATUS_REQUEST_COMPLETE:
-			return status_msg[15];
-		default:
-			return status_msg[16];
-	}
+	int running_handles = 0;
+	curl_multi_perform(hCurlMulti, &running_handles);
+	return running_handles == 1 ? "Completed" : "Running";
 }
 
 wiCriticalSection::wiCriticalSection(void* section)
-{ 
-#ifdef _ASYNC_MODE_
-	csection = section; 
-	EnterCriticalSection((LPCRITICAL_SECTION)csection); 
-#endif
+{
 }
 
 wiCriticalSection::~wiCriticalSection(void)
-{ 
-#ifdef _ASYNC_MODE_
-	LeaveCriticalSection((LPCRITICAL_SECTION)csection); 
-#endif
+{
 }
