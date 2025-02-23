f69813f60644767482712e6551b7c1ff963b229a
Solant <runner62v6@gmail.com>
2025-02-23 23:06:09 +0100

    fix: apply delta time to active part rotation

--- MechoSoma/iScreen/I-World.h
+++ MechoSoma/iScreen/I-World.h
@@ -234,7 +234,7 @@ class iWorldDispatcher
 	void SetAssemblyCamera(void);
 
 	void draw_AssemblyScreen(void);
-	void AssemblyQuant(void);
+	void AssemblyQuant(const float dt);
 	void AssemblyPartsQuant(int mode = 1);
 	void AssemblyCoordsQuant(void);
 
